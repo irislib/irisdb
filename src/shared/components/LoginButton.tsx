1da81575dcdf9bb5a8e1d736c8299437ffaab8d0
Martti Malmi <sirius@iki.fi>
2024-03-05 09:56:50 +0200

    logout

--- src/shared/components/LoginButton.tsx
+++ src/shared/components/LoginButton.tsx
@@ -1,24 +1,30 @@
-import {useState} from "react";
 import {requestPermission} from "@/shared/ndk.ts";
+import useLocalState from "@/state/useLocalState.ts";
 
 export default function LoginButton() {
-  const [publicKey, setPublicKey] = useState('');
+  const [publicKey, setPublicKey] = useLocalState('publicKey','');
 
   function login() {
     requestPermission().then((user) => {
       setPublicKey(user.npub);
     });
   }
+
+  function logout() {
+    setPublicKey('');
+  }
+
   return (
-    <>
+    <div className="mb-4 flex flex-row items-center gap-2 justify-between">
       {!publicKey && (
         <button className="btn btn-sm btn-accent mb-4" onClick={() => login()}>Nostr Extension Login</button>
       )}
       {publicKey && (
-        <div className="mb-4">
+        <>
           <div className="text-sm">Your public key: {publicKey}</div>
-        </div>
+          <button className="btn btn-sm btn-accent" onClick={() => logout()}>Log out</button>
+        </>
       )}
-    </>
+    </div>
   )
 }
