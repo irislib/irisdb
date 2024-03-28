02527037e139458512f4cfde41d7c18e4e3c2ebc
Martti Malmi <sirius@iki.fi>
2024-03-28 11:53:29 +0200

    rm nostr-hooks

--- iris-docs/src/app/index.tsx
+++ iris-docs/src/app/index.tsx
@@ -1,6 +1,5 @@
 import './index.css';
 
-import { NostrHooksContextProvider } from 'nostr-hooks';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/pages';
@@ -8,9 +7,7 @@ import { router } from '@/pages';
 export const App = () => {
   return (
     <>
-      <NostrHooksContextProvider>
-        <RouterProvider router={router} />
-      </NostrHooksContextProvider>
+      <RouterProvider router={router} />
     </>
   );
 };
