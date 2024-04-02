fb128519da538fa4eef32521149d860b01c0d1bd
Martti Malmi <sirius@iki.fi>
2024-04-02 19:09:02 +0300

    scroll up on location change

--- iris-docs/src/shared/components/Layout.tsx
+++ iris-docs/src/shared/components/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import Header from '@/shared/components/Header';
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
