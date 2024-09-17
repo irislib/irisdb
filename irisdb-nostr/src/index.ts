7bb6907cc72cec395f98cc7f934466c9888ff586
Martti Malmi <sirius@iki.fi>
2024-09-17 16:06:59 +0300

    SocialGraph to separate package, v1.0.18

--- irisdb-nostr/src/index.ts
+++ irisdb-nostr/src/index.ts
@@ -3,5 +3,3 @@ export { PublicKey } from './Hex/PublicKey';
 export { DEFAULT_RELAYS, ndk, newUserLogin, privateKeyLogin } from './ndk';
 export { default as NDKAdapter } from './NDKAdapter';
 export { default as publicState } from './publicState';
-export { SocialGraph } from './SocialGraph/SocialGraph';
-export { UniqueIds } from './SocialGraph/UniqueIds';
