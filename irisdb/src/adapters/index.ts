00d97a3d751131c17ec8dc8416c56106fb8f4ede
Martti Malmi <sirius@iki.fi>
2025-02-22 22:29:25 +0200

    v0.0.14

--- irisdb/src/adapters/index.ts
+++ irisdb/src/adapters/index.ts
@@ -3,5 +3,6 @@
  * Different storage and networking options for IrisDB.
  */
 export { BroadcastChannelAdapter } from './BroadcastChannelAdapter';
+export { IndexedDBAdapter } from './IndexedDBAdapter';
 export { LocalStorageMemoryAdapter } from './LocalStorageMemoryAdapter';
 export { MemoryAdapter } from './MemoryAdapter';
